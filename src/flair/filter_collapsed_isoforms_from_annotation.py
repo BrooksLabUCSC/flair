fd5852e5473a581667f079a87e05bc0f7fb31269
jeltje <yinnah@yahoo.com>
2023-08-28 12:14:55 -0700

    pull simple functions out of filter_collapsed_isoforms_from_annotation function

--- src/flair/filter_collapsed_isoforms_from_annotation.py
+++ src/flair/filter_collapsed_isoforms_from_annotation.py
@@ -34,96 +34,96 @@ def main():
 			support=args.support, queryfile=args.psl, outputfile=args.output, wiggle=args.wiggle,
 			new_map=args.new_map, isbed=isbed)
 
+
+
+def get_info(line, isbed):
+	if isbed:
+		chrom, name, chrstart = line[0], line[3], int(line[1])
+		sizes = [int(n) for n in line[10].split(',')[:-1]]
+		starts = [int(n) + chrstart for n in line[11].split(',')[:-1]]
+	else:
+		chrom, name = line[13], line[9]
+		sizes = [int(n) for n in line[18].split(',')[:-1]]
+		starts = [int(n) for n in line[20].split(',')[:-1]]
+	return chrom, name, sizes, starts
+
+
+def get_junctions(starts, sizes):
+	junctions = set()
+	if len(starts) != 1:
+		for b in range(len(starts)-1):
+			junctions.add((starts[b]+sizes[b], starts[b+1]))
+		return junctions
+
+
+def get_exons(starts, sizes):
+	exons = []
+	for e in range(len(starts)):
+		exons += [(starts[e], starts[e]+sizes[e])]
+	return exons
+
+
+def exon_overlap(coords0, coords1, left=True, tol=1):
+	len0, len1 = coords0[1] - coords0[0], coords1[1] - coords1[0]
+	if left:
+		return coords0[1] == coords1[1] and (len0 - tol) < len1
+	return coords0[0] == coords1[0] and (len0 - tol) < len1
+
+
+def contained(coords0, coords1):  # complete coverage of coords0 by coords1
+	return coords1[0] <= coords0[0] and coords1[1] >= coords0[1]
+
+
+def bin_search_left(query, data):
+	""" Query is a coordinate interval. Approximate binary search for the left coordinate of
+	the query in data sorted by the right coordinate. Finishes when the first interval in data with
+	a right coordinate that is greater than the query's left coordinate is found. """
+	lower, upper, i = 0, len(data), int(math.floor(len(data)/2))  # binary search prep
+	if upper == 0:
+		return set()
+	while True:
+		if lower == i or upper == i:
+			if i - 1 > 0 and data[i-1][1] >= query[0]:
+				lower = i = i - 1
+			else:
+				if data[i][1] < query[0]:
+					i = i + 1
+				break
+		elif data[i][1] < query[0]:  # i is too low of an index
+			lower = i
+			i = int(math.floor((lower + upper)/2.))  # floor because of python 2 and 3 differences
+		else: # i is too high of an index
+			upper = i
+			i = int(math.floor((lower + upper)/2.))
+	return data[i:min(i+40, len(data))]
+
+
+def bin_search_right(query, data):
+	""" Query is a coordinate interval. Approximate binary search for the left coordinate of
+	the query in data sorted by the right coordinate. Finishes when the first interval in data with
+	a left coordinate that is greater than the query's right coordinate is found. Kept separate for
+	my own readability. """
+	lower, upper, i = 0, len(data), int(math.floor(len(data)/2))
+	if upper == 0:
+		return set()
+	while True:
+		if lower == i or upper == i:
+			if i + 1 < len(data) and data[i+1][0] <= query[1]:
+				upper = i = i + 1
+			else:
+				break
+		elif data[i][0] < query[1]:
+			lower = i
+			i = int(math.floor((lower + upper)/2.))
+		else:
+			upper = i
+			i = int(math.floor((lower + upper)/2.))
+	return data[max(0, i-40):i+1]
+
 def filter_collapsed_isoforms_from_annotation(annotation, map_a, map_i, support, queryfile, outputfile,
 	wiggle=100, new_map=False, isbed=True):
 	psl = open(queryfile)
 	annotated = open(annotation)
-
-	def get_info(line, isbed):
-		if isbed:
-			chrom, name, chrstart = line[0], line[3], int(line[1])
-			sizes = [int(n) for n in line[10].split(',')[:-1]]
-			starts = [int(n) + chrstart for n in line[11].split(',')[:-1]]
-		else:
-			chrom, name = line[13], line[9]
-			sizes = [int(n) for n in line[18].split(',')[:-1]]
-			starts = [int(n) for n in line[20].split(',')[:-1]]
-		return chrom, name, sizes, starts
-
-
-	def get_junctions(starts, sizes):
-		junctions = set()
-		if len(starts) != 1:
-			for b in range(len(starts)-1):
-				junctions.add((starts[b]+sizes[b], starts[b+1]))
-			return junctions
-
-
-	def get_exons(starts, sizes):
-		exons = []
-		for e in range(len(starts)):
-			exons += [(starts[e], starts[e]+sizes[e])]
-		return exons
-
-
-	def exon_overlap(coords0, coords1, left=True, tol=1):
-		len0, len1 = coords0[1] - coords0[0], coords1[1] - coords1[0]
-		if left:
-			return coords0[1] == coords1[1] and (len0 - tol) < len1
-		return coords0[0] == coords1[0] and (len0 - tol) < len1
-
-
-	def contained(coords0, coords1):  # complete coverage of coords0 by coords1
-		return coords1[0] <= coords0[0] and coords1[1] >= coords0[1]
-
-
-	def bin_search_left(query, data):
-		""" Query is a coordinate interval. Approximate binary search for the left coordinate of
-		the query in data sorted by the right coordinate. Finishes when the first interval in data with
-		a right coordinate that is greater than the query's left coordinate is found. """
-		lower, upper, i = 0, len(data), int(math.floor(len(data)/2))  # binary search prep
-		if upper == 0:
-			return set()
-		while True:
-			if lower == i or upper == i:
-				if i - 1 > 0 and data[i-1][1] >= query[0]:
-					lower = i = i - 1
-				else:
-					if data[i][1] < query[0]:
-						i = i + 1
-					break
-			elif data[i][1] < query[0]:  # i is too low of an index
-				lower = i
-				i = int(math.floor((lower + upper)/2.))  # floor because of python 2 and 3 differences
-			else: # i is too high of an index
-				upper = i
-				i = int(math.floor((lower + upper)/2.))
-		return data[i:min(i+40, len(data))]
-
-
-	def bin_search_right(query, data):
-		""" Query is a coordinate interval. Approximate binary search for the left coordinate of
-		the query in data sorted by the right coordinate. Finishes when the first interval in data with
-		a left coordinate that is greater than the query's right coordinate is found. Kept separate for
-		my own readability. """
-		lower, upper, i = 0, len(data), int(math.floor(len(data)/2))
-		if upper == 0:
-			return set()
-		while True:
-			if lower == i or upper == i:
-				if i + 1 < len(data) and data[i+1][0] <= query[1]:
-					upper = i = i + 1
-				else:
-					break
-			elif data[i][0] < query[1]:
-				lower = i
-				i = int(math.floor((lower + upper)/2.))
-			else:
-				upper = i
-				i = int(math.floor((lower + upper)/2.))
-		return data[max(0, i-40):i+1]
-
-
 	iso_support = {}
 	annotated_iso_read_map = {}
 	flair_iso_read_map = {}
