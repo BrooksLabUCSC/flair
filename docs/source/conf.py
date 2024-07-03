f20e123f9983c2fc6d6ca0c4c7a1bc29658156fa
Jeltje <jeltje.van.baren@gmail.com>
2024-07-03 12:52:48 -0700

    debugging readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,6 +28,7 @@ author = 'Alison Tang, Cameron Soulette, Jeltje van Baren, Kevyn Hart, Eva Hrabe
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
