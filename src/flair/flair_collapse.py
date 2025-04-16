9a3812289fe1d9b1032af38315c32c2cc3177bd7
Colette Felton <cafelton@ucsc.edu>
2025-04-16 12:05:59 -0700

    edited collapse to resolve issue #466

--- src/flair/flair_collapse.py
+++ src/flair/flair_collapse.py
@@ -262,10 +262,10 @@ def collapse(genomic_range='', corrected_reads=''):
 			sys.stderr.write('ERROR: not enough reads in', precollapse)
 			sys.exit(1)
 
-	if args.annotation_reliant:
+	if args.annotation_reliant or args.remove_internal_priming:
 		if not args.generate_map:
 			args.generate_map = True
-		if args.annotation_reliant == 'generate' or not args.annotated_bed:
+		if (not args.annotation_reliant and args.remove_internal_priming) or args.annotation_reliant == 'generate' or not args.annotated_bed:
 			if not os.path.exists(args.gtf):
 				if not args.gtf:
 					sys.stderr.write('Please specify annotated gtf with -f for --annotation_reliant generate\n')
@@ -279,7 +279,7 @@ def collapse(genomic_range='', corrected_reads=''):
 			# gtf to bed
 			gtf_to_bed(args.annotated_bed, args.gtf, include_gene=True)
 
-		if args.annotation_reliant == 'generate':
+		if (not args.annotation_reliant and args.remove_internal_priming) or args.annotation_reliant == 'generate':
 			# get transcript sequences
 			args.annotation_reliant = args.output+'annotated_transcripts.fa'
 			bed_to_sequence(query=args.output+'annotated_transcripts.bed', genome=args.genome,
@@ -305,7 +305,7 @@ def collapse(genomic_range='', corrected_reads=''):
 		if args.remove_internal_priming:
 			count_cmd += ['--remove_internal_priming', '--intprimingthreshold', str(args.intprimingthreshold),
 						  '--intprimingfracAs', str(args.intprimingfracAs), '--transcriptomefasta',
-						  args.transcriptfasta]
+						  args.annotation_reliant]
 
 		if not args.quiet:
 			sys.stderr.write('Aligning reads to reference transcripts\n')
@@ -404,7 +404,7 @@ def collapse(genomic_range='', corrected_reads=''):
 		count_cmd += ['--generate_map', args.output+'isoform.read.map.txt']
 	if args.remove_internal_priming:
 		count_cmd += ['--remove_internal_priming', '--intprimingthreshold', str(args.intprimingthreshold),
-					  '--intprimingfracAs', str(args.intprimingfracAs), '--transcriptomefasta', args.transcriptfasta]
+					  '--intprimingfracAs', str(args.intprimingfracAs), '--transcriptomefasta', args.annotation_reliant]
 	if not args.quiet:
 		sys.stderr.write('Aligning reads to firstpass transcripts\n')
 		sys.stderr.write('Counting supporting reads for firstpass transcripts\n')
