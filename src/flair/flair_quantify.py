6c7c1d2516000e051004508885df57e28dfda35a
Colette Felton <cafelton@ucsc.edu>
2025-06-10 09:19:39 -0700

    restructured quantify to handle each sample separately and not store alignments

--- src/flair/flair_quantify.py
+++ src/flair/flair_quantify.py
@@ -83,30 +83,30 @@ def quantify(isoform_sequences=''):
                     subprocess.check_call(['mkdir', '-p', args.temp_dir])
                 readFileRoot = args.temp_dir + '/' + readFileRoot[readFileRoot.rfind('/')+1:]
 
-            if not os.path.exists(cols[3]):
-                raise Exception('Query file path does not exist: {}'.format(cols[3]))
+            if not os.path.exists(readFile):
+                raise Exception('Query file path does not exist: {}'.format(readFile))
 
             samData.append(cols + [readFileRoot + '.sam'])
     sys.stderr.write('Writing temporary files with prefixes similar to {}\n'.format(readFileRoot))
 
-    for num, sample in enumerate(samData, 0):
-        sys.stderr.write('Step 1/3. Aligning sample %s_%s, %s/%s \n' % (sample[0], sample[2], num+1, len(samData)))
-        mm2_command = ['minimap2', '--MD', '-a', '-N', '4', '-t', str(args.t), args.i, sample[-2]]
+    for num, data in enumerate(samData, 0):
+        sample, group, batch, readFile, samOut = data
+        sys.stderr.write('Aligning sample %s_%s, %s/%s \n' % (sample, batch, num+1, len(samData)))
+        mm2_command = ['minimap2', '--MD', '-a', '-N', '4', '-t', str(args.t), args.i, readFile]
 
         # TODO: Replace this with proper try/except Exception as ex
         try:
-            if subprocess.call(mm2_command, stdout=open(sample[-1], 'w'),
-                               stderr=open(sample[-1]+'.mm2_stderr.txt', 'w')):
-                raise Exception('Check {} file'.format(sample[-1]+'.mm2_stderr.txt'))
+            if subprocess.call(mm2_command, stdout=open(samOut, 'w'),
+                               stderr=open(samOut+'.mm2_stderr.txt', 'w')):
+                raise Exception('Check {} file'.format(samOut+'.mm2_stderr.txt'))
         except:
             raise Exception('''Possible minimap2 error, please check that all file, directory and executable paths exist''')
-        subprocess.check_call(['rm', sample[-1]+'.mm2_stderr.txt'])
+        subprocess.check_call(['rm', samOut+'.mm2_stderr.txt'])
         sys.stderr.flush()
 
-    countData = dict()
-    for num, data in enumerate(samData):
-        sample, group, batch, readFile, samOut = data
-        sys.stderr.write('Step 2/3. Quantifying isoforms for sample %s_%s: %s/%s \n' % (sample, batch, num+1, len(samData)))
+
+
+        sys.stderr.write('Quantifying isoforms for sample %s_%s: %s/%s \n' % (sample, batch, num+1, len(samData)))
 
         count_cmd = ['count_sam_transcripts.py', '-s', samOut,
                      '-o', samOut+'.counts.txt', '-t', str(args.t), '--quality', str(args.quality)]
@@ -122,16 +122,10 @@ def quantify(isoform_sequences=''):
             count_cmd += ['--generate_map', args.o+'.'+sample+'.'+group+'.isoform.read.map.txt']
 
         subprocess.check_call(count_cmd)
-        for line in open(samOut+'.counts.txt'):
-            line = line.rstrip().split('\t')
-            iso, numreads = line[0], line[1]
-            if iso not in countData:
-                countData[iso] = np.zeros(len(samData))
-            countData[iso][num] = numreads
         sys.stderr.flush()
 
         if args.output_bam:
-            sys.stderr.write('Step 2.5/3. Filtering bam reads for sample %s_%s: %s/%s \r' % (sample, batch, num+1, len(samData)))
+            sys.stderr.write('Filtering bam reads for sample %s_%s: %s/%s \r' % (sample, batch, num+1, len(samData)))
             readToIso = {}
             for line in open(args.o+'.'+sample+'.'+group+'.isoform.read.map.txt'):
                 line = line.rstrip().split('\t', 1)
@@ -161,12 +155,23 @@ def quantify(isoform_sequences=''):
                 newsam.write('\t'.join(line))
             newsam.close()
             subprocess.check_call(['samtools', 'sort', '-@', str(args.t), samOut.split('.sam')[0] + '-filtered.sam', '-o', args.o+'.'+sample+'.'+group+'.flair.aligned.bam'])
-
             subprocess.check_call(['samtools', 'index', args.o+'.'+sample+'.'+group+'.flair.aligned.bam'])
 
         subprocess.check_call(['rm', samOut])
 
-    sys.stderr.write('Step 3/3. Writing counts to {} \n'.format(args.o+'.counts.tsv'))
+    sys.stderr.write('Writing counts to {} \n'.format(args.o + '.counts.tsv'))
+    countData = dict()
+    for num, data in enumerate(samData):
+        sample, group, batch, readFile, samOut = data
+        for line in open(samOut+'.counts.txt'):
+            line = line.rstrip().split('\t')
+            iso, numreads = line[0], line[1]
+            if iso not in countData:
+                countData[iso] = np.zeros(len(samData))
+            countData[iso][num] = numreads
+
+
+
     countMatrix = open(args.o+'.counts.tsv', 'w')
 
     if args.sample_id_only:
