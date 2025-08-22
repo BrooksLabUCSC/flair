b81216f783f8f5a3cb184dd1c2998a2ab5cf4f9b
Colette Felton <cafelton@ucsc.edu>
2025-08-22 10:36:09 -0700

    continue fixing help messages, make tests work

--- src/flair/flair_diffSplice.py
+++ src/flair/flair_diffSplice.py
@@ -19,13 +19,13 @@ def diffSplice(isoforms='', counts_matrix=''):
     parser = argparse.ArgumentParser()
     required = parser.add_argument_group('required named arguments')
     if not isoforms:
-        required.add_argument('-i', '--isoforms', action='store', dest='i', required=True,
+        required.add_argument('-i', '--isoforms', action='store', required=True,
                 type=str, help='isoforms in bed format')
-        required.add_argument('-q', '--counts_matrix', action='store', dest='q',
+        required.add_argument('-q', '--counts_matrix', action='store',
                 type=str, required=True, help='tab-delimited isoform count matrix from flair quantify module')
-    required.add_argument('-o', '--out_dir', action='store', dest='o',
+    required.add_argument('-o', '--out_dir', action='store',
             type=str, required=True, help='Output directory for tables and plots.')
-    parser.add_argument('-t', '--threads', action='store', dest='t',
+    parser.add_argument('-t', '--threads', action='store',
             type=int, required=False, default=4, help='Number of threads for parallel DRIMSeq (4)')
     parser.add_argument('--test', action='store_true', dest='test',
             required=False, default=False, help='Run DRIMSeq statistical testing')
@@ -49,46 +49,46 @@ def diffSplice(isoforms='', counts_matrix=''):
     parser.add_argument('--conditionB', action='store', dest='conditionB', required=False, default='',
             help='''Specify another condition corresponding to samples in the counts_matrix to be compared against
             conditionA''')
-    parser.add_argument('-of', '--out_dir_force', action='store_true', dest='of',
+    parser.add_argument('-of', '--out_dir_force', action='store_true',
             required=False, help='''Specify this argument to force overwriting of files in
             an existing output directory''')
     args = parser.parse_args()
 
     if isoforms:
-        args.i = isoforms
-        args.q = counts_matrix
+        args.isoforms = isoforms
+        args.counts_matrix = counts_matrix
 
-    if not os.path.exists(args.q):
+    if not os.path.exists(args.counts_matrix):
         raise FlairInputDataError('Counts matrix file path does not exist')
-    if not os.path.exists(args.i):
+    if not os.path.exists(args.isoforms):
         raise FlairInputDataError('Isoform bed file path does not exist')
 
     # Create output directory including a working directory for intermediate files.
-    workdir = os.path.join(args.o, 'workdir')
-    if args.of:
+    workdir = os.path.join(args.out_dir, 'workdir')
+    if args.out_dir_force:
         if not os.path.exists(workdir):
             os.makedirs(workdir)
         pass
-    elif not os.path.exists(args.o):
+    elif not os.path.exists(args.out_dir):
         try:
             os.makedirs(workdir, 0o700)
         except OSError as ex:
             raise OSError("** ERROR cannot create directory %s" % (workdir)) from ex
     else:
-        raise FlairInputDataError(f'** Error. Name {args.o} already exists. Choose another name for out_dir')
-    if args.i.endswith('psl'):
+        raise FlairInputDataError(f'** Error. Name {args.out_dir} already exists. Choose another name for out_dir')
+    if args.isoforms.endswith('psl'):
         raise FlairInputDataError('** Error. Flair no longer accepts PSL input. Please use psl_to_bed first.')
 
-    filebase = os.path.join(args.o, 'diffsplice')
-    pipettor.run(['call_diffsplice_events.py', args.i, filebase, args.q])
-    pipettor.run(['es_as.py', args.i], stdout=open(filebase+'.es.events.tsv', 'w'))
-    pipettor.run(['es_as_inc_excl_to_counts.py', args.q, filebase+'.es.events.tsv'],
+    filebase = os.path.join(args.out_dir, 'diffsplice')
+    pipettor.run(['call_diffsplice_events.py', args.isoforms, filebase, args.counts_matrix])
+    pipettor.run(['es_as.py', args.isoforms], stdout=open(filebase+'.es.events.tsv', 'w'))
+    pipettor.run(['es_as_inc_excl_to_counts.py', args.counts_matrix, filebase+'.es.events.tsv'],
                  stdout=open(filebase+'.es.events.quant.tsv', 'w'))
     os.unlink(filebase+'.es.events.tsv')
 
     if args.test or args.conditionA:
         logging.info('DRIMSeq testing for each AS event type')
-        ds_command = ['Rscript', diffSplice_drimSeq, '--threads', args.t, '--outDir', args.o,
+        ds_command = ['Rscript', diffSplice_drimSeq, '--threads', args.threads, '--outDir', args.out_dir,
                       '--drim1', args.drim1, '--drim2', args.drim2, '--drim3', args.drim3, '--drim4', args.drim4]
         if args.batch:
             ds_command += ['--batch']
